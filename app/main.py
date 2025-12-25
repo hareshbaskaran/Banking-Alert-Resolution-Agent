df86fba73d9061cd8dd46965aadb9bcba8d0d068
ad1207 <rsadithya12@gmail.com>
2025-12-25 22:26:23 +0530

    fix : gitignore pycache files

--- app/main.py
+++ app/main.py
@@ -2,37 +2,37 @@ from app.graph import workflow
 
 def run():
     alerts = [
-        # {
-        #     "alert_id": "ALT-001",
-        #     "scenario_code": "A-001",
-        #     "subject_id": "CUST-101",   # Trader without prior velocity spike
-        #     "subject_name": "Alice Trader"
-        # },
+        {
+            "alert_id": "ALT-001",
+            "scenario_code": "A-001",
+            "subject_id": "CUST-101",   # Trader without prior velocity spike
+            "subject_name": "Alice Trader"
+        },
 
-        # {
-        #     "alert_id": "ALT-002",
-        #     "scenario_code": "A-002",
-        #     "subject_id": "CUST-102",   # Cash business with linked accounts
-        #     "subject_name": "Raj Retail"
-        # },
-        # {
-        #     "alert_id": "ALT-003",
-        #     "scenario_code": "A-003",
-        #     "subject_id": "CUST-103",   # Teacher wiring to metals
-        #     "subject_name": "Charlie Teach"
-        # },
-        # {
-        #     "alert_id": "ALT-004",
-        #     "scenario_code": "A-004",
-        #     "subject_id": "CUST-108",   # Sanctioned counterparty
-        #     "subject_name": "Unknown Entity"
-        # },
-        # {
-        #     "alert_id": "ALT-005",
-        #     "scenario_code": "A-005",
-        #     "subject_id": "CUST-109",   # Dormant account reactivation
-        #     "subject_name": "Elder Retiree"
-        # }
+        {
+            "alert_id": "ALT-002",
+            "scenario_code": "A-002",
+            "subject_id": "CUST-102",   # Cash business with linked accounts
+            "subject_name": "Raj Retail"
+        },
+        {
+            "alert_id": "ALT-003",
+            "scenario_code": "A-003",
+            "subject_id": "CUST-103",   # Teacher wiring to metals
+            "subject_name": "Charlie Teach"
+        },
+        {
+            "alert_id": "ALT-004",
+            "scenario_code": "A-004",
+            "subject_id": "CUST-108",   # Sanctioned counterparty
+            "subject_name": "Unknown Entity"
+        },
+        {
+            "alert_id": "ALT-005",
+            "scenario_code": "A-005",
+            "subject_id": "CUST-109",   # Dormant account reactivation
+            "subject_name": "Elder Retiree"
+        }
     ]
 
     for alert in alerts:
